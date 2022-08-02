2d1c417d6a6134719325d98158073c7f659437f7
Stygian84 <nicholasgandhi84@gmail.com>
2022-08-02 12:25:09 +0800

    Notify user through email

--- server/main.go
+++ server/main.go
@@ -1,14 +1,20 @@
 package main
 
+import (
+	"esc/ascendaRoyaltyPoint/pkg/config"
+	"esc/ascendaRoyaltyPoint/pkg/controllers"
+	"esc/ascendaRoyaltyPoint/pkg/models"
+	"log"
+)
+
 func main() {
-	// config.Connect()
-	// db := config.GetDB()
-	// store := models.NewStore(db)
-	// server := controllers.NewServer(store)
-	// err := server.Start("0.0.0.0:8080")
-	// if err != nil {
-	// 	log.Fatal("cannot start server", err)
-	// }
+	config.Connect()
+	db := config.GetDB()
+	store := models.NewStore(db)
+	server := controllers.NewServer(store)
+	err := server.Start("0.0.0.0:8080")
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
 	// handle.RunCron("01:00", "03:00")
-	//handle.SendAccrual()
 }
