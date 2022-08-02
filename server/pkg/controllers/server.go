48f3426162451255c29fbe2f5993f3918a80ae5b
ChehKangMing <kangmingpc@gmail.com>
2022-08-02 15:57:30 +0800

    updated routes

--- server/pkg/controllers/server.go
+++ server/pkg/controllers/server.go
@@ -1,31 +1,39 @@
 package controllers
 
-import(
-  "esc/ascendaRoyaltyPoint/pkg/models"
-  "github.com/gin-gonic/gin"
+import (
+	"esc/ascendaRoyaltyPoint/pkg/models"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
-type Server struct{
-  store *models.Store
-  router *gin.Engine
+
+type Server struct {
+	store  *models.Store
+	router *gin.Engine
 }
 
-func (server *Server) Start(address string)error{
-  return server.router.Run(address)
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
 
 }
 
-func NewServer(store *models.Store) *Server{
-  server := &Server{
-    store:store,
-  }
-  router := gin.Default()
+func NewServer(store *models.Store) *Server {
+	server := &Server{
+		store: store,
+	}
+	router := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowAllOrigins = true
+	router.Use(cors.New(config))
 
-  router.POST("/initTransaction",server.CreateTransaction)
-  router.POST("/loyalty/validateMembership", server.CheckLoyaltyRegEx)
-  router.POST("/loyalty", server.CreateLoyaltyProg)
-  router.GET("/loyalty", server.GetLoyalty)
-  router.GET("/loyalty/:id", server.GetLoyaltyId)
-  router.POST("/checkReward",server.CheckRewardRate)
-  server.router=router
-  return server
+	router.POST("/initTransaction", server.CreateTransaction)
+	router.POST("/loyalty/validateMembership", server.CheckLoyaltyRegEx)
+	router.POST("/loyalty", server.CreateLoyaltyProg)
+	router.GET("/loyalty", server.GetLoyalty)
+	router.GET("/loyalty/:id", server.GetLoyaltyId)
+	router.POST("/checkReward", server.CheckRewardRate)
+
+	router.GET("/transaction_status/:id", server.GetAllCreditRequest)
+	server.router = router
+	return server
 }
